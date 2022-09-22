a3a38dd6301db6848c60f6ff96375d4d98d8ee97
julian <juliandecastelli@gmail.com>
2022-09-22 16:40:52 -0300

    add sound effect

--- index.js
+++ index.js
@@ -13,43 +13,62 @@ class Explosion {
     this.spriteHeight = 179;
     this.widht = this.spriteWidth * 0.7;
     this.height = this.spriteHeight * 0.7;
-    this.x = x - this.widht * 0.5;
-    this.y = y - this.height * 0.5;
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = "boom.png";
     this.frame = 0;
     this.timer = 0;
+    this.angle = Math.random() * 6.2;
+    this.sound = new Audio();
+    this.sound.src = "woosh.wav";
   }
   update() {
+    if (this.frame === 0) {
+      this.sound.currentTime = 0.12;
+      this.sound.play();
+    }
+
     this.timer++;
     if (this.timer % frames === 0) {
       this.frame++;
     }
   }
   draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
     ctx.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.widht * 0.5,
+      0 - this.height * 0.5,
       this.widht,
       this.widht
     );
+    ctx.restore();
   }
 }
 window.addEventListener("click", (e) => {
+  createAnimation(e);
+});
+function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
   let positionY = e.y - canvasPosition.top;
   explosions.push(new Explosion(positionX, positionY));
-});
+}
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   for (let i = 0; i < explosions.length; i++) {
     explosions[i].update();
     explosions[i].draw();
+    if (explosions[i] > 5) {
+      explosions.splice(i, 1);
+      i--;
+    }
   }
   requestAnimationFrame(animate);
 }
